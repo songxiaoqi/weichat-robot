c82dc0931b5e3690ccc3c0abf89074cc7982654f
songxiaoqi <wosongxiaoqi@163.com>
2019-07-02 14:12:55 +0800

    Initial commit

---
+++ src/main/java/com/ypwh/robot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ypwh.robot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WeichatRobotApplication.class);
+    }
+
+}
---
+++ src/main/java/com/ypwh/robot/WeichatRobotApplication.java
@@ -0,0 +1,13 @@
+package com.ypwh.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeichatRobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeichatRobotApplication.class, args);
+    }
+
+}
